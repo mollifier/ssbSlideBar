a7f15600127dc4868610c10ce10e2561121e5ffb
mollifier <mollifier@gmail.com>
2009-09-05 12:17:41 +0900

    Add input buttons.

--- ssbSlideBar.js
+++ ssbSlideBar.js
@@ -1,7 +1,14 @@
 jetpack.future.import("slideBar");
 
 jetpack.slideBar.append({
-  html: "<h1>slideBar sample</h1>",
+  html: "<h1>stack stock books slideBar</h1>" +
+        "<div>" +
+          "<input type='button' value='unread' />" +
+          "<input type='button' value='reading' />" +
+          "<input type='button' value='read' />" +
+          "<input type='button' value='wish' />" +
+        "</div>",
+
   width: 350,
   onSelect: function(s) {
     s.slide(350, {persist: true});
