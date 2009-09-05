232b7157618f26918736446a938ee32cf9e80d92
mollifier <mollifier@gmail.com>
2009-09-05 11:41:47 +0900

    Add onSelect handler.

--- ssbSlideBar.js
+++ ssbSlideBar.js
@@ -2,7 +2,10 @@ jetpack.future.import("slideBar");
 
 jetpack.slideBar.append({
   html: "<h1>slideBar sample</h1>",
-  width: 350
+  width: 350,
+  onSelect: function(s) {
+    s.slide(350, {persist: true});
+  }
 });
 
 // vim:set ts=2 sw=2:
